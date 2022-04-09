2e040cbea41a81ea21accc3fb2a1780436e77d03
Oloko Adejoke <olokojoke@gmail.com>
2022-04-08 20:08:36 -0700

    Add if else conditions

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,18 +7,30 @@
  *
  * Description - Prints a number and its last digit along with whether its
  * its last digit is 0, greater than 5, or less than 6 and not 0.
- * return: 0
+ * Return: Always 0 (success)
  */
-int main (void)
+int main(void)
 {
 int n;
+int last_digit;
 
 srand (time(0));
 n = rand() - RAND_MAX / 2
+last digit = n % 10
+  if (last digit == 10)
+    {
+     printf("Last digit of %d is 0 and is 0\n", n);
+    }
+  else
+    {
 
-printf("last digit of %d is %d %s\n", n, n % 10)
-((n % 10) == 0) ? "and is 0"
-: (((n % 10) > 5) ? "and is greater than 5"
-: "and is less than 6 and not 0"));
-
-return(0)
+     if (last_digit > 5)
+       {
+	printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+       }
+     else
+       {
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+       }
+    }
+ return (0);
