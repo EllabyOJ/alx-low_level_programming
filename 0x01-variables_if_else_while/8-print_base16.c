690ccea053772f0d35925eab239a1577a44ca88a
Oloko Adejoke <olokojoke@gmail.com>
2022-04-11 19:31:21 -0700

    Removed whitespace

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,17 +7,17 @@
 int main(void)
 
 {
-  char base_16;
-  for (base_16 = 48; base_16 <= 57; base_16++)
-    {
-      putchar(base_16);
-    }
-  for (base_16 = 97 ; base_16 <= 102; base_16++)
-    {
-      putchar(base_16);
-    }
-  putchar ('\n');
+char base_16;
+for (base_16 = 48; base_16 <= 57; base_16++)
+{
+putchar(base_16);
+}
+for (base_16 = 97 ; base_16 <= 102; base_16++)
+{
+putchar(base_16);
+}
+putchar ('\n');
 
-  return (0);
+return (0);
 
 }
